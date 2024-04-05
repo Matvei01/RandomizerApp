b45438ed8aab8c72489ca0d50cbd16fc3983187f
Matvei <khlestov.matvei@mail.ru>
2024-04-05 23:07:23 +0500

    Created base project, removed storyboard references

---
+++ RandomizerApp/RandomizerApp/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RandomizerApp
+//
+//  Created by Matvei Khlestov on 05.04.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
